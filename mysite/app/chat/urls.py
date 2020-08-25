f21ca85894ec4c80e547efd8f47086c1837595c5
elaineyeh <10456018@ntub.edu.tw>
2020-08-26 00:48:18 +0800

    fix: mysite and chat urls

--- mysite/app/chat/urls.py
+++ mysite/app/chat/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import include
 from django.urls import path
-from django.contrib import admin
+
+from . import views
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
